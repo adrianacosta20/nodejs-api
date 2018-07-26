6a809b00c61dea5b0ee84442b779d61f1a1509c6
Adrian Acosta <aacosta@techlaunch>
2018-07-25 22:07:01 -0400

    completed lions CRUD API & removed lodash

--- server/server.js
+++ server/server.js
@@ -3,20 +3,70 @@ var app = express();
 var port = 9091;
 var host = '127.0.0.1';
 
-var jsonData = { count: 12, message : 'hey'};
+app.use(express.static('client'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req,res)=>{
-    //res.sendFile takes an absolute path to a file and sets the mime type based on file extension
-    res.sendfile(__dirname+'/index.html', (err)=>{
-        if(err) res.status(500).send(err);
+var lions = [
+    {
+        'id': 1,
+        'name': 'Simba',
+        'pride': 'The cool cats',
+        'age': 3,
+        'gender': 'male'
+    },
+    {
+        'id': 2,
+        'name': 'Mufasa',
+        'pride': 'The bad cats',
+        'age': 4,
+        'gender': 'male'
+    }
+];
+var id = 2;
+
+app.get('/lion', (req,res)=>{
+    res.json(lions);
+});
+
+app.get('/lions/:id', (req,res)=>{
+    var lion = lions.find(lion=>{
+        return lion.id == req.params.id;
     });
+    res.json(lion || {});
 });
 
-app.get('data', (res,req)=>{
-    res.json(jsonData);
+app.post('/lions', (req,res)=>{
+    var lion = req.body;
+    i++;
+    lion.id = id+'';
+    lions.push(lion);
 });
 
-app.listen(port, host, () =>{
-    console.log('Listening on http://localhost:',port);
+app.put('/lions/:id', (req,res)=>{
+    var update = req.body;
+    if(update.id) delete update.id;
+    var lion = lions.findIndex(lion=> lion.id = req.params.id);    
+    if(!lions[lion]){
+        res.send();
+    }else{
+        var updatedLion = Object.assign(lions[lion], updated);
+        res.json(updatedLion);
+    };
+});
+
+app.delete('/lions/:id', (req,res)=>{
+    var lion = lions.findIndex(lion=> lion.id == req.params.id);
+    if(!lions[lion]){
+        res.send();
+    }else{
+        var deletedLion = lions[lion];
+        lions.splice(lion,1);
+        res.json(deletedLion);
+    };
+});
+
+app.listen(port, host, () => {
+    console.log('Listening on http://localhost:', port);
 
 });
