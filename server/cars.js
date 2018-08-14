7accd5417f4b9172264d591d5a0e0d6798a3de4a
Adrian Acosta <aacosta@techlaunch>
2018-08-13 21:01:31 -0400

    routes added

---
+++ server/cars.js
@@ -0,0 +1,18 @@
+var cars = [
+    {
+        'id': 1,
+        'name': 'Toyota',
+        'description': 'Japanese car',
+        'year': 2010,
+    },
+    {
+        'id': 2,
+        'name': 'Ford',
+        'description': 'American car',
+        'year': 2004
+    }
+];
+
+module.exports = {
+    cars:cars
+};
--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var morgan = require('morgan');
 var port = 9091;
 var host = '127.0.0.1';
+var cars = require('./cars').cars;
+
+// app.use(morgan('combined'));
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9091');
@@ -16,20 +20,7 @@ app.use(express.static(__dirname+'/../client'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var cars = [
-    {
-        'id': 1,
-        'name': 'Toyota',
-        'description': 'Japanese car',
-        'year': 2010,
-    },
-    {
-        'id': 2,
-        'name': 'Ford',
-        'description': 'American car',
-        'year': 2004
-    }
-];
+
 
 var id = 2;
 
@@ -41,41 +32,44 @@ app.get('/get-cars', (req,res)=>{
     res.json(cars);
 });
 
-app.get('/cars/:id', (req,res)=>{
-    var car = cars.find(car=>{
-        return car.id == req.params.id;
-    });
-    res.json(lion || {});
+// app.get('/cars/:id', (req,res)=>{
+//     var car = cars.find(car=>{
+//         return car.id == req.params.id;
+//     });
+//     res.json(lion || {});
+// });
+
+app.post('/add-car', (req,res)=>{
+    var car = req.body;
+    console.log(car);
+    id++;
+    car.id = id;
+    cars.push(car);
+    res.send(car);
 });
 
-app.post('/lions', (req,res)=>{
-    var lion = req.body;
-    i++;
-    lion.id = id+'';
-    lions.push(lion);
-});
+// app.put('/lions/:id', (req,res)=>{
+//     var update = req.body;
+//     if(update.id) delete update.id;
+//     var lion = lions.findIndex(lion=> lion.id = req.params.id);    
+//     if(!lions[lion]){
+//         res.send();
+//     }else{
+//         var updatedLion = Object.assign(lions[lion], updated);
+//         res.json(updatedLion);
+//     };
+// });
 
-app.put('/lions/:id', (req,res)=>{
-    var update = req.body;
-    if(update.id) delete update.id;
-    var lion = lions.findIndex(lion=> lion.id = req.params.id);    
-    if(!lions[lion]){
-        res.send();
-    }else{
-        var updatedLion = Object.assign(lions[lion], updated);
-        res.json(updatedLion);
-    };
-});
-
-app.delete('/lions/:id', (req,res)=>{
-    var lion = lions.findIndex(lion=> lion.id == req.params.id);
-    if(!lions[lion]){
-        res.send();
-    }else{
-        var deletedLion = lions[lion];
-        lions.splice(lion,1);
-        res.json(deletedLion);
-    };
+app.delete('/delete-car', (req,res)=>{
+    console.log(req.body);
+    // var lion = lions.findIndex(lion=> lion.id == req.params.id);
+    // if(!lions[lion]){
+    //     res.send();
+    // }else{
+    //     var deletedLion = lions[lion];
+    //     lions.splice(lion,1);
+    //     res.json(deletedLion);
+    // };
 });
 
 app.listen(port, host, () => {
